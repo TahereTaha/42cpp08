dbb2728b8eac470421d4bda5b362a409b1481890
Taha Tahere <elberkani.taha@gmail.com>
2026-02-12 04:55:30 +0100

    done the ex02 mutant stack

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack(void) {}
+	MutantStack(const MutantStack &src) : std::stack<T>(src) {}
+	MutantStack &operator = (const MutantStack &src)
+	{
+		std::stack<T>::operator=(src);
+		return (*this);
+	}
+	~MutantStack() {}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin()
+	{
+		return (this->c.begin());
+	}
+
+	iterator end()
+	{
+		return (this->c.end());
+	}
+
+	const_iterator begin() const 
+	{
+		return (this->c.begin());
+	}
+	
+	const_iterator end() const 
+	{
+		return (this->c.end());
+	}
+};
+
